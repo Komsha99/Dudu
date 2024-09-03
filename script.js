ff70a22d31eb83041f13ab564cac10b67e2652b6
Vlada1234 <zivkovicv107@gmail.com>
2024-09-03 10:00:46 +0200

    ...

--- script.js
+++ script.js
@@ -21,63 +21,78 @@ observer.observe(hero);
 
 
 
-const carousel = document.querySelector(".carousel");
-const arrowBtns = document.querySelectorAll(".wrapper i");
-const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-const carouselChildrens = [...carousel.children];
+let carousel = document.querySelector(".carousel");
+let btns = document.querySelectorAll(".wrapper i");
+let carouselChildren = [...carousel.children];
+let wrapper = document.querySelector(".wrapper");
 
-let isDragging = false, startX, startScrollingLeft;
 
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+let cardWidth = carousel.querySelector(".card").offsetWidth;
+let isDragging = false,
+  startX,
+  startScrollLeft,
+  isAutoPlay = true,
+  timeoutId;
 
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+
+let cardsPerView = Math.round(carousel.offsetWidth / cardWidth);
+
+carouselChildren
+  .slice(-cardsPerView)
+  .reverse()
+  .forEach((card) => {
     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
+
+carouselChildren.slice(0, cardsPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
 
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id == "left" ? -cardWidth : cardWidth;
+  });
 });
 
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
-    })
-});
+let dragStart = (e) => {
+  isDragging = true;
 
-const dragStart = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
-    startX = e.pageX;
-    startScrollingLeft = carousel.scrollLeft;
-}
+  carousel.classList.add("dragging");
 
-const dragging = (e) => {
-    if(!isDragging) return;
-    carousel.scrollLeft = startScrollingLeft - (e.pageX - startX);
-}
+  
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
 
-const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove("dragging");
-}
+let dragging = (e) => {
 
-const infiniteScroll = () => {
-    if(carousel.scrollLeft === 0) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth);
-        carousel.classList.remove("no-transition");
-    }
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.offsetWidth;
-        carousel.classList.remove("no-transition");
-    }
-}
+  if (!isDragging) return;
+
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+let dragStop = () => {
+  isDragging = false;
+
+  carousel.classList.remove("dragging");
+};
+
+let infiniteScroll = () => {
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  } else if (
+    Math.ceil(carousel.scrollLeft) ===
+    carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+};
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop)
+document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
-
-
-
