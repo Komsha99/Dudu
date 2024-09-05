52692144ece4d04ad8fc1948701453aa518eda01
Komsha99 <58040179+Komsha99@users.noreply.github.com>
2024-09-05 12:48:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const header = document.querySelector('header');
 const hero = document.querySelector('.hero');
+const links = document.querySelectorAll('.nav-links li a');
+
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = this.getAttribute('href');
+    const targetElement = document.querySelector(target);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
 
 const options = {
     root: null, 
@@ -21,15 +36,15 @@ observer.observe(hero);
 
 $(document).ready(function(){
   var $carousel = $('.carousel').slick({
-    infinite: true,          
-    slidesToShow: 3,         
-    slidesToScroll: 1,       
-    autoplay: true,          
-    autoplaySpeed: 2000,     
-    arrows: true,            
-    draggable: true,         
-    prevArrow: '#left',      
-    nextArrow: '#right'      
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    draggable: true,
+    prevArrow: '#left',
+    nextArrow: '#right'
   });
 
 
@@ -44,4 +59,18 @@ $carousel.on('mousedown', function() {
 $carousel.on('mouseup mouseleave', function() {
   $carousel.slick('slickPlay');
 });
+});
+
+$(document).ready(function () {
+	$('body').on('click', '.services-card', function(){
+		var id = $(this).attr("id");
+		$('.contact-inputs').children('option').each(function (key, option) {
+			if(id == $(option).attr('value')){
+				$(option).attr('selected', 'selected');
+			}
+			else{
+				$(option).removeAttr('selected');
+			}
+		});
+	});
 });
